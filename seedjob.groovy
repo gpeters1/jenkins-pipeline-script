696fed5fbe10de24bb7784d997a2a3951281cad6
Gregory Peters <gregory.peters1@nhs.net>
2021-02-04 17:07:03 +0000

    gre

--- seedjob.groovy
+++ seedjob.groovy
@@ -1,19 +1,3 @@
-// seedjob.groovy
-// create an array with our two pipelines
-pipelines = ["greg-test"]
-// iterate through the array and call the create_pipeline method
-pipelines.each { pipeline ->
-    println "Creating pipeline ${pipeline}"
-    create_pipeline(pipeline)
-}
-// a method that creates a basic pipeline with the given parameter name
-def create_pipeline(String name) {
-    pipelineJob(name) {
-        definition {
-            cps {
-                sandbox(true)
-                script("""
-// this is an example declarative pipeline that says hello and goodbye
 pipeline {
     agent any
     stages {
